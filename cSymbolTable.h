63dd4b9bda092386b9ff6475ab67251291b7a5b8
Devon Andrade <andrade824@gmail.com>
2016-01-19 16:57:29 -0800

    Lab 3 *actually* completed

---
+++ cSymbolTable.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#include <list>
+#include <map>
+#include <string>
+#include "cSymbol.h"
+using std::list;
+using std::map;
+using std::string;
+
+class cSymbolTable
+{
+public:
+    // Default Constructor
+    cSymbolTable();
+
+    // Copy Constructor
+    cSymbolTable(const cSymbolTable & copy);
+
+    // Assignment Op overload
+    cSymbolTable & operator=(const cSymbolTable & rhs);
+
+    // Increase the scope of the symbol table
+    void IncreaseScope();
+
+    // Decreaset he scope of the symbol table
+    void DecreaseScope();
+
+    // Insert a symbol into the current scope if it doesn't already exist
+    cSymbol * Insert(const string & name);
+
+    // Lookup a symbol only within the current symbol table
+    cSymbol * LookupInner(const string & name) const;
+
+    // Lookup a symbol within every symbol table (every scope).
+    cSymbol * Lookup(const string & name) const;
+private:
+    list<map<string, cSymbol *>> m_scopes;
+};
